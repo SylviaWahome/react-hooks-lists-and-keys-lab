c77dc352bab214f67e3a58223dfc29b982f99326
Shee-001 <sylviawanjiru804@gmail.com>
2025-07-11 08:41:35 +0300

    Pass all React Lists

--- src/components/App.js
+++ src/components/App.js
@@ -3,15 +3,35 @@ import NavBar from "./NavBar";
 import Home from "./Home";
 import About from "./About";
 import ProjectList from "./ProjectList";
-import user from "../data/user";
+
+const projectData = [
+  {
+    id: 1,
+    name: "Shee Parlour Website",
+    about: "A nature-inspired beauty salon website",
+    technologies: ["HTML", "CSS", "JavaScript"]
+  },
+  {
+    id: 2,
+    name: "Blog Manager",
+    about: "A CRUD app for managing blog posts",
+    technologies: ["React", "JSON Server", "Bootstrap"]
+  },
+  {
+    id: 3,
+    name: "Library System",
+    about: "A single-page app to manage books and users",
+    technologies: ["JavaScript", "HTML", "JSON"]
+  }
+];
 
 function App() {
   return (
     <div>
       <NavBar />
-      <Home username={user.name} city={user.city} color={user.color} />
-      <About bio={user.bio} links={user.links} />
-      <ProjectList projects={user.projects} />
+      <Home />
+      <About />
+      <ProjectList projects={projectData} />
     </div>
   );
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,7 @@
 import React from "react";
 
-function Home({ color, username, city }) {
-  return (
-    <div id="home">
-      <h1 style={{ color: color }}>
-        {username} is a Web Developer from {city}
-      </h1>
-    </div>
-  );
+function Home() {
+  return <h1>Welcome to My Portfolio</h1>;
 }
 
 export default Home;
